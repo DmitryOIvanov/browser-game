8b4377fcca2ae9a8337cc4487eb427f41f983657
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2025-11-09 22:27:32 -0500

    ring shooter fundamentals WIP

--- enemies/ringShooter.js
+++ enemies/ringShooter.js
@@ -1,11 +1,11 @@
-import { CircleArea, RegularPolygonArea } from "../areas.js";
+import { RegularPolygonArea } from "../areas.js";
 import { createDefenseProfile } from "../attackAndDefense.js";
 import Color from "../color.js";
 import { ctx } from "../drawing.js";
 import { bounceBoundify, normalizedAtan2, randomAngle } from "../extraMath.js";
 import ExplodingRingParticle from "../particles/explodingRingParticle.js";
 import playField from "../playField.js";
-import AbstractBasicCircle from "./abstractBasicCircle.js";
+import BallEProj from "../projectiles/enemy/ballEProj.js";
 import AbstractEnemy from "./abstractEnemy.js";
 
 const BODY_RAD = 32;
@@ -15,15 +15,25 @@ const PUPIL_RAD = 8;
 const PUPIL_MAX_OFFSET = 9;
 const NUM_SIDES = 5;
 const ROT_SPEED = 0.03;
-const LOOK_BASE_TIME = 5;
-const LOOK_TIME_VAR = 20;
-const LOOK_RANGE = 0.5 * Math.PI;
-const DIRECT_LOOK_CHANCE = 0.7;
-const LOOK_SWITCH_TIME = 5;
+
+const BULLET_RAD = 6;
+const BULLET_LINE_THICK = 6;
+const NUM_BULLETS = 10;
+const BULLET_SKIP = 3;
+const BULLET_CREATE_TIME = 10;
+const RING_ROT_SPEED = 0.02;
+
+const STATE_IDLE = 0;
+const STATE_BUILD = 1;
+const STATE_SHOOT = 2;
+const STATE_BASE_TIMES = [60, 60, 30];
+const STATE_TIME_VARS = [0, 0, 0];
 
 const LINE_THICK = 6;
 const MAX_HP = 20;
 const HIT_FLASH_TIME = 2;
+
+const dummyProjectile = new BallEProj(0, 0, 0, 0, BULLET_RAD, BULLET_LINE_THICK);
 
 export default class RingShooter extends AbstractEnemy {
 	static RAD = BODY_RAD;
@@ -37,15 +47,11 @@ export default class RingShooter extends AbstractEnemy {
 		this.rotDir = Math.random() > 0.5 ? 1 : -1;
 		this.area = new RegularPolygonArea(x, y, BODY_RAD, this.bodyRot, NUM_SIDES);
 		this.defenseProfile = createDefenseProfile(MAX_HP);
-		this.lookCountdown = 0;
-		this.look = {
-			x1: 0,
-			y1: 0,
-			x2: 0,
-			y2: 0
-		};
-		this.lookSwitch = 1;
-		this.directLook = true;
+		this.state = STATE_IDLE;
+		this.stateProgress = 0;
+		this.stateDuration = 0;
+
+		this.bulletProgress = new Array(NUM_BULLETS).fill(-1);
 	}
 
 	timeStep(dt) {
@@ -54,30 +60,23 @@ export default class RingShooter extends AbstractEnemy {
 		if (this.hitFlash < 0) this.hitFlash = 0;
 
 		this.bodyRot += ROT_SPEED * this.rotDir * dt;
-		this.lookSwitch += dt / LOOK_SWITCH_TIME;
 
-		this.lookCountdown -= dt;
-		while (this.lookCountdown <= 0) {
-			this.look.x1 = this.look.x2;
-			this.look.y1 = this.look.y2;
-			this.directLook = (Math.random() <= DIRECT_LOOK_CHANCE);
+		this.stateProgress += dt;
+		while (true) {
+			if (this.state == STATE_IDLE) {
+				if (this.stateProgress >= this.stateDuration) {
+					this.stateProgress -= this.stateDuration;
+					this.state = STATE_BUILD;
+					this.stateDuration = getStateDuration(this.state);
+					continue;
+				}
+			} else if (this.state == STATE_BUILD) {
 
-			if (!this.directLook) {
-				let angle = Math.atan2(playField.player.y - this.y, playField.player.x - this.x);
-				angle += (2 * Math.random() - 1) * LOOK_RANGE;
-				this.look.x2 = PUPIL_MAX_OFFSET * Math.cos(angle);
-				this.look.y2 = PUPIL_MAX_OFFSET * Math.sin(angle);
+			} else if (this.state == STATE_SHOOT) {
 			}
+			break;
+		}
 
-			this.lookSwitch = -this.lookCountdown;
-			this.lookCountdown += LOOK_BASE_TIME + LOOK_TIME_VAR * Math.random();
-		}
-		if (this.lookSwitch > 1) this.lookSwitch = 1;
-		if (this.directLook) {
-			const angle = Math.atan2(playField.player.y - this.y, playField.player.x - this.x);
-			this.look.x2 = PUPIL_MAX_OFFSET * Math.cos(angle);
-			this.look.y2 = PUPIL_MAX_OFFSET * Math.sin(angle);
-		}
 
 		this.area.update(this.x, this.y, BODY_RAD, this.bodyRot, NUM_SIDES);
 	}
@@ -106,8 +105,9 @@ export default class RingShooter extends AbstractEnemy {
 		ctx.stroke();
 
 		ctx.fillStyle = ctx.strokeStyle;
-		const pupilX = this.x + this.look.x1 + this.lookSwitch * (this.look.x2 - this.look.x1);
-		const pupilY = this.y + this.look.y1 + this.lookSwitch * (this.look.y2 - this.look.y1);
+		const playerAngle = Math.atan2(playField.player.y - this.y, playField.player.x - this.x);
+		const pupilX = this.x + PUPIL_MAX_OFFSET * Math.cos(playerAngle);
+		const pupilY = this.y + PUPIL_MAX_OFFSET * Math.sin(playerAngle);
 		ctx.beginPath();
 		ctx.arc(pupilX, pupilY, PUPIL_RAD, 0, 2 * Math.PI);
 		ctx.closePath();
