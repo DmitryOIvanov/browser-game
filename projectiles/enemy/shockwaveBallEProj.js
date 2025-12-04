7fb770c7b334572890c051ba037fd803d9f4a60d
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2025-12-03 21:37:45 -0500

    shockwave spawns all possible bullets

--- projectiles/enemy/shockwaveBallEProj.js
+++ projectiles/enemy/shockwaveBallEProj.js
@@ -1,8 +1,14 @@
 import { CircleArea } from "../../areas.js";
-import { ctx } from "../../drawing.js";
-import { isInBounds } from "../../extraMath.js";
+import { canv, ctx } from "../../drawing.js";
+import { isInBounds, normalizeAngle } from "../../extraMath.js";
 import playField from "../../playField.js";
 import BallEProj from "./ballEProj.js";
+
+const PLAYFIELD_CENTER_X = canv.width / 2;
+const PLAYFIELD_CENTER_Y = canv.height / 2;
+const PLAYFIELD_OUTER_RAD_SQR = PLAYFIELD_CENTER_X * PLAYFIELD_CENTER_X + PLAYFIELD_CENTER_Y * PLAYFIELD_CENTER_Y;
+const PLAYFIELD_OUTER_RAD = Math.sqrt(PLAYFIELD_OUTER_RAD_SQR);
+const PLAYFIELD_INNER_RAD_SQR = PLAYFIELD_CENTER_Y * PLAYFIELD_CENTER_Y;
 
 export default class ShockwaveBallEProj {
 	constructor(x, y, angle, speed, rad, shockAngle, shockSpeed, shockRad, shockPeriod, thick, color) {
@@ -13,10 +19,8 @@ export default class ShockwaveBallEProj {
 		this.vy = speed * Math.sin(angle);
 		this.angle = angle;
 		this.rad = rad;
-		this.shockVX1 = shockSpeed * Math.cos(angle + shockAngle);
-		this.shockVY1 = shockSpeed * Math.sin(angle + shockAngle);
-		this.shockVX2 = shockSpeed * Math.cos(angle - shockAngle);
-		this.shockVY2 = shockSpeed * Math.sin(angle - shockAngle);
+		this.shockSpeed = shockSpeed;
+		this.shockAngle = shockAngle;
 		this.shockRad = shockRad;
 		this.shockPeriod = shockPeriod;
 		this.thick = thick;
@@ -26,15 +30,32 @@ export default class ShockwaveBallEProj {
 		this.area = new CircleArea(this.x, this.y, this.rad);
 	}
 
+	isImpotent() {
+		const dx = PLAYFIELD_CENTER_X - this.x;
+		const dy = PLAYFIELD_CENTER_Y - this.y;
+		const dSqr = dx * dx + dy * dy;
+		if (dSqr <= PLAYFIELD_OUTER_RAD_SQR) return false;
+		const angleToCenter = Math.atan2(dy, dx);
+		const acceptableDeviation = Math.atan(PLAYFIELD_OUTER_RAD / Math.sqrt(dSqr));
+		console.log(this.shockAngle + acceptableDeviation, normalizeAngle(angleToCenter - this.angle), normalizeAngle(this.angle - angleToCenter));
+		if (normalizeAngle(angleToCenter - this.angle) < this.shockAngle + acceptableDeviation) return false;
+		if (normalizeAngle(this.angle - angleToCenter) < this.shockAngle + acceptableDeviation) return false;
+		return true;
+	}
+
 	timeStep(dt) {
 		this.shockCountdown -= dt;
 		while (this.shockCountdown <= 0) {
 			const shockX = this.x - this.vx * this.shockCountdown;
 			const shockY = this.y - this.vy * this.shockCountdown;
-			const proj1 = new BallEProj(shockX, shockY, this.shockVX1, this.shockVY1, this.shockRad, this.thick, this.color);
+			const shockVX1 = this.shockSpeed * Math.cos(this.angle + this.shockAngle);
+			const shockVY1 = this.shockSpeed * Math.sin(this.angle + this.shockAngle);
+			const shockVX2 = this.shockSpeed * Math.cos(this.angle - this.shockAngle);
+			const shockVY2 = this.shockSpeed * Math.sin(this.angle - this.shockAngle);
+			const proj1 = new BallEProj(shockX, shockY, shockVX1, shockVY1, this.shockRad, this.thick, this.color);
 			proj1.timeStep(-this.shockCountdown);
 			playField.addEnemyProjectile(proj1);
-			const proj2 = new BallEProj(shockX, shockY, this.shockVX2, this.shockVY2, this.shockRad, this.thick, this.color);
+			const proj2 = new BallEProj(shockX, shockY, shockVX2, shockVY2, this.shockRad, this.thick, this.color);
 			proj2.timeStep(-this.shockCountdown);
 			playField.addEnemyProjectile(proj2);
 			this.shockCountdown += this.shockPeriod;
@@ -42,12 +63,7 @@ export default class ShockwaveBallEProj {
 
 		this.x += this.vx * dt;
 		this.y += this.vy * dt;
-		if (
-			!isInBounds(this.x, playField.x, -this.rad) ||
-			!isInBounds(this.y, playField.y, -this.rad)
-		) {
-			this.retired = true;
-		}
+		if (this.isImpotent()) this.retired = true;
 		this.area.update(this.x, this.y, this.rad);
 	}
 
