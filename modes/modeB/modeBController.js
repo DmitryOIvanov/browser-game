3b32a399781e7783d5543466a11d06cbd5f750d6
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2026-02-03 19:14:15 -0500

    tutorial prompt in mode B

--- modes/modeB/modeBController.js
+++ modes/modeB/modeBController.js
@@ -4,11 +4,13 @@ import playField from "../../playField.js";
 import ModeBManager from "./modeBManager.js";
 import SimpleOptionScreen from "../../simpleOptionScreen.js";
 import modeBLevels from "./modeBLevels.js";
+import { TutorialTask } from "../../tutorial/tutorialTask.js";
 
 const STATE_WEAPON_SELECT = 0;
 const STATE_PLAYING = 1;
 const STATE_LOSE = 2;
 const STATE_END = 3;
+const STATE_TUTORIAL_DECISION = 4;
 
 export default class ModeBController {
     constructor() {
@@ -16,7 +18,7 @@ export default class ModeBController {
         this.subController = null;
 
         this.levelReached = 0;
-        this.startPlay();
+        this.startTutorialDecision();
     }
 
     nextFrame() {
@@ -47,6 +49,14 @@ export default class ModeBController {
             if (this.subController.concluded) {
                 this.concluded = true;
             }
+        } else if (this.state == STATE_TUTORIAL_DECISION) {
+            this.subController.nextFrame();
+            if (this.subController.concluded) {
+                if (this.subController.result != 0) {
+                    this.levelReached = 2;
+                }
+                this.startPlay();
+            }
         }
     }
 
@@ -71,5 +81,12 @@ export default class ModeBController {
         controls.mouse.leftHeld = false;
         this.state = STATE_END;
         this.subController = new SimpleMessageScreen("You Win");
+    }
+
+    startTutorialDecision() {
+        controls.mouse.lPressed = false;
+        controls.mouse.leftHeld = false;
+        this.state = STATE_TUTORIAL_DECISION;
+        this.subController = new SimpleOptionScreen("Play Tutorial?", ["Yes", "No"]);
     }
 }
