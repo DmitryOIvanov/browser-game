d01a36ac4020f9870c51af958629917e9d5d16fb
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2026-03-14 21:19:04 -0400

    removed redundant argument in requestanimation...

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ let showDebugInfo = false;
 controller.initialize();
 
 const fpsTracker = new FpsTracker();
-function renderLoop(){
+function renderLoop() {
     fpsTracker.startProductiveFrame();
 
     controller.nextFrame();
 
-    if(controls.pressed["KeyI"]) showDebugInfo = !showDebugInfo;
-    if(showDebugInfo){
+    if (controls.pressed["KeyI"]) showDebugInfo = !showDebugInfo;
+    if (showDebugInfo) {
         ctx.textAlign = "left";
         ctx.font = "36px serif";
         ctx.fillStyle = '#fff';
         const latestFpsInfo = fpsTracker.getLatestFinishedSampleInfo();
-        ctx.fillText(latestFpsInfo.realFPSText, 30,50);
-        ctx.fillText(latestFpsInfo.projectedFPSText, 30,90);
+        ctx.fillText(latestFpsInfo.realFPSText, 30, 50);
+        ctx.fillText(latestFpsInfo.projectedFPSText, 30, 90);
         ctx.font = "24px serif";
-        ctx.fillText("Particles",30,120); ctx.fillText(`${playField.particles.length}`,160,120);
-        ctx.fillText("Enemies",30,144); ctx.fillText(`${playField.enemies.length}`,160,144);
-        ctx.fillText("Enemy Proj.",30,168); ctx.fillText(`${playField.enemyProj.length}`,160,168);
-        ctx.fillText("Player Proj.",30,192); ctx.fillText(`${playField.playerProj.length}`,160,192);
+        ctx.fillText("Particles", 30, 120); ctx.fillText(`${playField.particles.length}`, 160, 120);
+        ctx.fillText("Enemies", 30, 144); ctx.fillText(`${playField.enemies.length}`, 160, 144);
+        ctx.fillText("Enemy Proj.", 30, 168); ctx.fillText(`${playField.enemyProj.length}`, 160, 168);
+        ctx.fillText("Player Proj.", 30, 192); ctx.fillText(`${playField.playerProj.length}`, 160, 192);
     }
 
     controls.clearPresses();
-    requestAnimationFrame(renderLoop, canv);
+    requestAnimationFrame(renderLoop);
 
     fpsTracker.endFrame();
 }
