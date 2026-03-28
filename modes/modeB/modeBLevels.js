f420b7aa26ff9e7ffe911b95e489ea52b8b7ab96
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2026-03-27 23:23:12 -0400

    tutorial task list deduplicated

--- modes/modeB/modeBLevels.js
+++ modes/modeB/modeBLevels.js
@@ -5,6 +5,7 @@ import CheckpointTask from "../../tasks/checkpointTask.js";
 import { ShootTutorialTask } from "../../tutorial/shootTutorialTask.js";
 import { SpecialTutorialTask } from "../../tutorial/specialTutorialTask.js";
 import TimeSlowTutorialTask from "../../tutorial/timeSlowTutorialTask.js";
+import { TUTORIAL_TASK_LIST } from "../../tutorial/tutorialTaskList.js";
 import { WasdTutorialTask } from "../../tutorial/wasdTutorialTask.js";
 
 function getLevelIntroMessage(number) {
@@ -32,149 +33,6 @@ const TEMP_END_MESSAGE = {
     fadeOutTime: 30,
     opacity: 0.6,
 }
-
-const tutorialPart1 = [
-    {
-        class: ExecuteFunctionTask,
-        function: () => {
-            playField.player.weapon.disableSecondary();
-        }
-    },
-    {
-        class: CreateBgParticleAndWaitForRetirementTask,
-        particleClass: WasdTutorialTask
-    },
-    {
-        class: CreateBgParticleAndWaitForRetirementTask,
-        particleClass: ShootTutorialTask
-    },
-    {
-        class: CreateMessageTask,
-        centerX: canv.width / 2,
-        centerY: canv.height / 2 - 200,
-        text: "Defeat all Enemies",
-        fontSizePx: 60,
-        fadeInTime: 30,
-        showTime: 210,
-        fadeOutTime: 180,
-        opacity: 0.6,
-    },
-    {
-        class: WaitTimeTask,
-        time: 90
-    },
-    {
-        class: CreateMessageTask,
-        centerX: canv.width / 2,
-        centerY: canv.height / 2 + 200,
-        text: "5 Hits and You're Out",
-        fontSizePx: 50,
-        fadeInTime: 30,
-        showTime: 120,
-        fadeOutTime: 180,
-        opacity: 0.6,
-    },
-    {
-        class: WaitTimeTask,
-        time: 90
-    },
-    {
-        class: WeightedSpawnTask,
-        delayCoeff: 30,
-        enemies: [
-            { name: "SmallSquare", weight: 1, num: 3 },
-            { name: "SmallTriangle", weight: 1, num: 3 },
-            { name: "SmallCircle", weight: 1.2, num: 3 },
-        ]
-    },
-    { class: CheckpointTask },
-];
-
-const tutorialPart2 = [
-    {
-        class: ExecuteFunctionTask,
-        function: () => {
-            playField.player.weapon.enableSecondary();
-        }
-    },
-    {
-        class: CreateBgParticleAndWaitForRetirementTask,
-        particleClass: SpecialTutorialTask
-    },
-    {
-        class: CreateBgParticleAndWaitForRetirementTask,
-        particleClass: TimeSlowTutorialTask
-    },
-    {
-        class: CreateMessageTask,
-        centerX: canv.width / 2,
-        centerY: canv.height / 2 - 200,
-        text: "Try out Your New Abilities",
-        fontSizePx: 60,
-        fadeInTime: 30,
-        showTime: 120,
-        fadeOutTime: 180,
-        opacity: 0.6,
-    },
-    {
-        class: WaitTimeTask,
-        time: 60
-    },
-    {
-        class: WeightedSpawnTask,
-        delayCoeff: 30,
-        enemies: [
-            {
-                shuffle: [
-                    { name: "SmallSquare", weight: 1, num: 3 },
-                    { name: "SmallTriangle", weight: 1, num: 3 },
-                    { name: "SmallCircle", weight: 1, num: 3 },
-                ]
-            },
-            {
-                shuffle: [
-                    { name: "SmallSquare", weight: 1, num: 3 },
-                    { name: "SmallTriangle", weight: 1, num: 3 },
-                    { name: "SmallCircle", weight: 1, num: 3 },
-                    { name: "MultiSquare", weight: 2, num: 2 },
-                    { name: "MultiTriangle", weight: 2, num: 2 },
-                    { name: "MultiCircle", weight: 2, num: 1 },
-                ]
-            }
-        ]
-    },
-    { class: CheckpointTask },
-    {
-        class: CreateMessageTask,
-        centerX: canv.width / 2,
-        centerY: canv.height / 2 - 200,
-        text: "Tutorial Complete",
-        fontSizePx: 60,
-        fadeInTime: 30,
-        showTime: 210,
-        fadeOutTime: 45,
-        opacity: 0.6,
-    },
-    {
-        class: WaitTimeTask,
-        time: 90
-    },
-    {
-        class: CreateMessageTask,
-        centerX: canv.width / 2,
-        centerY: canv.height / 2 + 200,
-        text: "Good Luck",
-        fontSizePx: 50,
-        fadeInTime: 30,
-        showTime: 120,
-        fadeOutTime: 45,
-        opacity: 0.6,
-    },
-    {
-        class: WaitTimeTask,
-        time: 195
-    },
-];
 
 const level1 = [
     getLevelIntroMessage(1),
@@ -443,8 +301,8 @@ const level6 = [
 ];
 
 const modeBLevels = [
-    tutorialPart1,
-    tutorialPart2,
+    TUTORIAL_TASK_LIST[0],
+    TUTORIAL_TASK_LIST[1],
     // level1,
     // level2,
     // level3,
