c3360c382f06e9974fe4263e58a97e1908650f00
Dmitry Ivanov <128776199+DmitryOIvanov@users.noreply.github.com>
2026-04-18 18:36:58 -0400

    commented batch file

--- bundle_and_zip.bat
+++ bundle_and_zip.bat
@@ -1,4 +1,11 @@
+:: Create a single "bundled_src.js" file using the instance of rollup installed locally by npm (see rollup.config.js)
 CALL npx rollup -c
+
+:: Reset the zip file if it exists already
 CALL 7z d attack_vector.zip
+
+:: Add html file, js file, and supplementary resources to zip
 CALL 7z a -tzip attack_vector.zip bundle_index.html bundled_src.js resources
+
+:: Inside the zip, rename each of the above as necessary and enclose them in a top-level directory
 CALL 7z rn attack_vector.zip bundle_index.html attack_vector/attack_vector.html bundled_src.js attack_vector/src.js resources attack_vector/resources
